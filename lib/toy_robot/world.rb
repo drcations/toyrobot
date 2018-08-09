e17190edfb57d5f847dc7924f0408e644f5a6f91
dave.cations <david@cations.me>
2018-08-09 21:43:42 +0100

    don't do anything if new coordinates are invalid

--- lib/toy_robot/world.rb
+++ lib/toy_robot/world.rb
@@ -53,3 +53,11 @@ module ToyRobot
             when 'WEST' then [@coords[0] - 1, @coords[1]]
             else raise "Wrong direction #{@direction}"
             end
+  
+            if self.class.valid_coords?(coords)
+              self.class.new(coords, @direction)
+            else
+              self
+            end
+          end
+        end
