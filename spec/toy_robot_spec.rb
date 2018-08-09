bed883b158b0f8382150437070c9c4137b90f424
dave.cations <david@cations.me>
2018-08-09 18:33:04 +0100

    describe - initialize

--- spec/toy_robot_spec.rb
+++ spec/toy_robot_spec.rb
@@ -2,3 +2,18 @@ RSpec.describe ToyRobot::Program do
     subject { instance }
   
     let(:instance) { described_class.new(text) }
+
+    describe '#initialize' do
+      let(:text) { 'PLACE 1,2,EAST MOVE MOVE LEFT MOVE REPORT' }
+  
+      it 'converts plaintext into a sequence commands' do
+        expect(subject.commands).to eq [
+          [:place, [1, 2], 'EAST'],
+          :move,
+          :move,
+          [:turn, :left],
+          :move,
+          :report
+        ]
+      end
+    end
