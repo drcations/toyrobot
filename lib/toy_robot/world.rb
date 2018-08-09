7b336a5897f59bf2192d4c6ec0e7e034c4a91fd0
dave.cations <david@cations.me>
2018-08-09 21:09:58 +0100

    this command requires arguments, so we consume another token

--- lib/toy_robot/world.rb
+++ lib/toy_robot/world.rb
@@ -15,4 +15,20 @@ module ToyRobot
   
           case token
           when 'PLACE'
+            
+            args = tokens.shift
+            x_string, y_string, direction = args.split(',')
+            x, y = [x_string, y_string].map(&:to_i)
+            @commands << [:place, [x, y], direction]
+          when 'MOVE'
+            @commands << :move
+          when 'LEFT'
+            @commands << [:turn, :left]
+          when 'RIGHT'
+            @commands << [:turn, :right]
+          when 'REPORT'
+            @commands << :report
+          end
+        end
+      end
   
